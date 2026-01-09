c7b2f0a12acd57b110d31875ea20542b20367430
Giorubio27 <Giorubio27@gmail.com>
2026-01-09 13:22:27 -0700

    I did exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        Console.Write("What is your grade percent? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        if (grade >= 90)
+        {
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+
+
+        }
+        else if (grade >= 80)
+        {
+
+            if (grade >= 80)
+            {
+                letter = "B";
+            }
+
+        }
+        else if (grade >= 70)
+        {
+
+            if (grade >= 70)
+            {
+                letter = "C";
+            }
+
+        }
+        else if (grade >= 60)
+        {
+
+            if (grade >= 60)
+            {
+                letter = "D";
+            }
+
+        }
+        else if (grade >= 50)
+        {
+
+            if (grade >= 50)
+            {
+                letter = "F";
+            }
+
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Nice try, Try again soon");
+        }
+        Console.WriteLine($"{letter}");
+
+
+
+
     }
 }

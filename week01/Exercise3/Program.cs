14e17691e2e6ed8972ed77396d62661e5d672576
Giorubio27 <Giorubio27@gmail.com>
2026-01-09 18:17:47 -0700

    I did the third exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.WriteLine("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int magicNumberMan = int.Parse(magicNumber);
+        Random randomNumberGenerator = new Random();
+        int magicNumber = randomNumberGenerator.Next(1, 100);
+        bool match = false;
+        while (match == false)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            int guessNumberMan = int.Parse(guessNumber);
+            if (magicNumber == guessNumberMan)
+            {
+                Console.Write("You guessed it right!.");
+                match = true;
+            }
+            else if (magicNumber > guessNumberMan)
+            {
+                Console.WriteLine("You guessed too low");
+            }
+            else if (magicNumber < guessNumberMan)
+            {
+                Console.WriteLine("You guessed too high. ");
+            }
+        }
+
+
+
     }
 }

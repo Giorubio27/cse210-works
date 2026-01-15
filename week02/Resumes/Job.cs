41ff5297c4313c3f1e5372abaf351afe1179468e
Giorubio27 <Giorubio27@gmail.com>
2026-01-15 09:24:38 -0700

    I finished with the rest of the activity

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    public string _jobTitle = "";
+    public string _jobCompany = "";
+    public string _startDate = "";
+    public string _endDate = "";
+    public void DisplayJobInfo()
+    {
+        Console.WriteLine($"{_jobTitle} ({_jobCompany}) {_startDate}-{_endDate}");
+
+    }
+    
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._jobCompany = "Microsoft";
+        job1._startDate = "2014";
+        job1._endDate = "2024";
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._jobCompany = "Apple";
+        job2._startDate = "2013";
+        job2._endDate = "2025";
+        
+
+        Resume resume1 = new Resume();
+        resume1._name = "Gary Snail";
+
+        
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
+
     }
 }
+

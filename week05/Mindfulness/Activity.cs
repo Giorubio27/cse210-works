b1eae60b1aeb70e8c2f153aae3eaaa51aa3655db
Giorubio27 <Giorubio27@gmail.com>
2026-02-06 07:25:45 -0700

    tabbed out

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+
+public class Activity
+{
+    private string _name;
+
+    private string _description;
+
+    private string _duration;
+
+    public Activity()
+    {
+
+    }
+    public void DisplayStartingMessage()
+    {
+
+    }
+    public void DisplayEndingMessage()
+    {
+
+    }
+    public void ShowSpinner()
+    {
+
+    }
+    public void ShowCountDown(int seconds)
+    {
+        
+    }
+}
